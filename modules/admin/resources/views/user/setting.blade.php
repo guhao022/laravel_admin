a59f99258d8e0db41d8d29c041d4974ee4e08202
guhao <378999587@qq.com>
2017-12-07 17:34:22 +0800

    haha

--- modules/admin/resources/views/user/setting.blade.php
+++ modules/admin/resources/views/user/setting.blade.php
@@ -1,8 +1,15 @@
 @extends('admin::layouts.app')
 @section('title', '用户设置')
+@section('pagetitle', '用户设置')
 
 @section('content')
 
-    //
+    <form method="get" class="form-horizontal">
 
+        <div class="form-group"><label class="col-sm-2 control-label">Normal</label>
+
+            <div class="col-sm-10"><input type="text" class="form-control"></div>
+        </div>
+
+    </form>
 @stop
