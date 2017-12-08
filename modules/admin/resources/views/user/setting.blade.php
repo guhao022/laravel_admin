a3b548001e2ecd159468c4f07eb75773f4972a7e
guhao <378999587@qq.com>
2017-12-08 09:21:52 +0800

    haha

--- modules/admin/resources/views/user/setting.blade.php
+++ modules/admin/resources/views/user/setting.blade.php
@@ -3,13 +3,16 @@
 @section('pagetitle', '用户设置')
 
 @section('content')
+    <div>
+        <form method="get" class="form-horizontal">
 
-    <form method="get" class="form-horizontal">
+            <div class="form-group"><label class="col-sm-2 control-label">Normal</label>
 
-        <div class="form-group"><label class="col-sm-2 control-label">Normal</label>
+                <div class="col-sm-10"><input type="text" class="form-control"></div>
+            </div>
 
-            <div class="col-sm-10"><input type="text" class="form-control"></div>
-        </div>
+        </form>
 
-    </form>
+    </div>
+
 @stop
