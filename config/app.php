d90194178465f10a6f76f64ddb53e2b066b0b8a4
guhao <378999587@qq.com>
2017-12-11 16:13:38 +0800

    haha

--- config/app.php
+++ config/app.php
@@ -229,7 +229,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
+        'Entrust' => Zizaco\Entrust\EntrustFacade::class,
         'Admin' => Modules\Admin\Facades\Admin::class,
 
     ],
