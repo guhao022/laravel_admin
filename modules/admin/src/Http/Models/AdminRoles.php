d0670fcecba3a904d02945c78919adc3c3e341b2
guhao <378999587@qq.com>
2017-12-11 16:13:57 +0800

    haha

--- modules/admin/src/Http/Models/AdminRoles.php
+++ modules/admin/src/Http/Models/AdminRoles.php
@@ -12,6 +12,5 @@ class AdminRoles extends EntrustRole
     public function users()
     {
         return $this->belongsToMany(config('admin.auth.providers.admin.model'), config('entrust.role_user_table'),config('entrust.role_foreign_key'),config('entrust.user_foreign_key'));
-        // return $this->belongsToMany(Config::get('auth.model'), Config::get('entrust.role_user_table'));
     }
 }
