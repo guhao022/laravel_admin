5515018870b4ccece8239a8e070979f37969eb1e
guhao <378999587@qq.com>
2017-12-13 08:41:24 +0800

    haha

--- modules/admin/database/seeds/AdminRolesTableSeeder.php
+++ modules/admin/database/seeds/AdminRolesTableSeeder.php
@@ -22,7 +22,7 @@ class AdminRolesTableSeeder extends Seeder
         ]);
 
         DB::table('admin_role_user')->insert([
-            'admin_id'=>1,
+            'user_id'=>1,
             'admin_roles_id'=>1,
         ]);
     }
