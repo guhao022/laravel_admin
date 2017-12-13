e0595c2eb37c9e7c1356d41d57540bff02211951
guhao <378999587@qq.com>
2017-12-13 17:36:51 +0800

    haha

--- modules/admin/resources/views/user/index.blade.php
+++ modules/admin/resources/views/user/index.blade.php
@@ -8,28 +8,28 @@
         <div class="col-lg-12">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <h5> 用户列表 </h5>
-                    <div class="ibox-tools">
+
+                    <div class="row form-inline pull-right">
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon">@</span>
+                                <input type="text" placeholder="Username" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon">@</span>
+                                <input type="text" placeholder="Username" class="form-control">
+                            </div>
+                        </div>
 
                     </div>
+
+                    <h5> 用户列表 </h5>
+
                 </div>
                 <div class="ibox-content">
-                    <div class="row form-inline">
-
-                        <div class="form-group">
-                            <div class="input-group">
-                                <span class="input-group-addon">@</span>
-                                <input type="text" placeholder="Username" class="form-control">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="input-group">
-                                <span class="input-group-addon">@</span>
-                                <input type="text" placeholder="Username" class="form-control">
-                            </div>
-                        </div>
-
-                    </div>
                     <div class="table-responsive">
                         <table class="table table-striped">
                             <thead>
