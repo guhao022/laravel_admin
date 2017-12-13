0ecc2aca71e16ea4934f793aead61f92796eec82
guhao <378999587@qq.com>
2017-12-13 09:59:48 +0800

    haha

---
+++ modules/admin/src/Services/AdminPermission.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ASUS-02
+ * Date: 2017/12/13
+ * Time: 9:57
+ */
+
+namespace Modules\Admin\Services;
+
+
+use Illuminate\Support\Facades\Route;
+use Modules\Admin\Models\AdminPermissions;
+use Modules\Admin\Repositories\RoleRepository;
+
+class AdminPermission
+{
+
+    public $roleRepository;
+    public $admin;
+    public function __construct(RoleRepository $roleRepository)
+    {
+        $this->roleRepository = $roleRepository;
+        $this->admin = auth()->guard('admin')->user();
+    }
+
+    public function permissionHtml()
+    {
+        $Permission = AdminPermissions::where('fid','0')->orWhere('name','like','%index')->get();
+
+        $treePermission = $this->roleRepository->tree($Permission);
+
+        $currentPermission = AdminPermissions::where('name',Route::currentRouteName())->first();
+
+        if(Route::currentRouteName() =='admin.home') {
+            $html = '<li class="active">
+                    <a href="/admin">
+                        <span class="icon fa fa-tachometer"></span><span class="title">控制面板</span>
+                    </a>
+                </li>';
+        }else{
+            $html = '<li>
+                    <a href="/admin">
+                        <span class="icon fa fa-tachometer"></span><span class="title">控制面板</span>
+                    </a>
+                </li>';
+        }
+
+        foreach($treePermission as $fatherPermission) {
+            if($this->admin->can($fatherPermission['name']) || $this->admin->hasRole('admin')) {
+
+                if(isset($currentPermission->fid) && $currentPermission->fid == $fatherPermission['id']){
+
+                    $fatherLiClass = 'panel panel-default dropdown active';
+                    $fatherDivClass = 'panel-collapse collapse in';
+                }else{
+                    $fatherLiClass = 'panel panel-default dropdown';
+                    $fatherDivClass = 'panel-collapse collapse';
+                }
+
+                $html .= '<li class="'.$fatherLiClass.'">
+                        <a data-toggle="collapse" href="#father-permission-'.$fatherPermission['id'].'">
+                        <span class="icon "></span><span class="title">'.$fatherPermission['display_name'].'</span>
+                        </a>';
+                if(count($fatherPermission['children']) > 0){
+                    $html .= '<div id="father-permission-'.$fatherPermission['id'].'" class="'.$fatherDivClass.'">
+                            <div class="panel-body">
+                                <ul class="nav navbar-nav">';
+                    foreach($fatherPermission['children'] as $childPermission){
+                        if($this->admin->can($childPermission['name']) || $this->admin->hasRole('admin')){
+                            if(isset($currentPermission->group_name) && $currentPermission->group_name == $childPermission['group_name']){
+                                $childStyle = 'color:#19B5FE';
+                            }else{
+                                $childStyle = '';
+                            }
+                            $html .= '<li><a style="'.$childStyle.'" href="'.route($childPermission['name']).'">'.$childPermission['display_name'].'</a></li>';
+                        }
+                    }
+                    $html .='</ul></div></div></li>';
+                }
+            }
+        }
+        return $html;
+    }
+
+}
