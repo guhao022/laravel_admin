97792b3253330d0ace3bd16ba248b8be8d8b39ba
guhao <378999587@qq.com>
2017-12-14 17:20:54 +0800

    haha

--- modules/admin/resources/views/adminlte/user/index.blade.php
+++ modules/admin/resources/views/adminlte/user/index.blade.php
@@ -7,71 +7,26 @@
             <div class="box-header with-border">
                 <h3 class="box-title"></h3>
 
-                <div class="pull-right">
-                    <div class="form-inline pull-right">
-                        <form action="" method="get">
-                            <fieldset>
-                                <div class="input-group input-group-sm">
-                                    <span class="input-group-addon"><strong>ID</strong></span>
-                                    <input type="text" class="form-control" placeholder="Id" name="id" value="">
-                                </div>
-
-                                <div class="btn-group btn-group-sm">
-                                    <button type="submit" class="btn btn-primary" title="搜索"><i class="fa fa-search"></i></button>
-                                    <a href="{{ route($current_menu->name) }}" class="btn btn-warning" title="还原"><i class="fa fa-undo"></i></a>
-                                </div>
-
-                            </fieldset>
-                        </form>
-                    </div>
-
-                </div>
-
                 <div class="btn-group" style="margin-right: 10px">
                     <a href="{{ route('admin.create') }}" class="btn btn-sm btn-success">
                         <i class="fa fa-save"></i>&nbsp;&nbsp;新增
                     </a>
                 </div>
             </div>
-            <div class="box-body table-responsive no-padding">
-                <table class="table table-hover">
-                    <tbody>
-                    <tr>
-                        <th><input type="checkbox" class="minimal-red grid-select-all"></th>
-                        <th>ID</th>
-                        <th>User</th>
-                        <th>Date</th>
-                        <th>Status</th>
-                        <th>Reason</th>
-                    </tr>
 
-                    <tr>
-                        <td>
-                            <input type="checkbox" class="minimal grid-row-checkbox" data-id="1" />
-                        </td>
-                        <td>183</td>
-                        <td>John Doe</td>
-                        <td>11-7-2014</td>
-                        <td><span class="label label-success">Approved</span></td>
-                        <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input type="checkbox" class="minimal grid-row-checkbox" data-id="1" />
-                        </td>
-                        <td>219</td>
-                        <td>Alexander Pierce</td>
-                        <td>11-7-2014</td>
-                        <td><span class="label label-warning">Pending</span></td>
-                        <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
+            <div class="box-body no-padding">
+    <ul class="users-list ">
+        <li>
+            <img src="{{admin_asset('img/user1-128x128.jpg')}}" alt="User Image">
+            <a class="users-list-name" href="#">Alexander Pierce</a>
+            <span class="users-list-date">Today</span>
+        </li>
+
+    </ul>
+    <!-- /.users-list -->
+</div>
 
         </div>
     </div>
-@stop
-@section('scripts')
 
-@stop
+    @stop
