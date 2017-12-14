cad7611dcb1441746e6a42e575a00640e45f020c
guhao <378999587@qq.com>
2017-12-14 09:41:13 +0800

    haha

--- modules/admin/database/migrations/2017_10_23_115237_add_routename_pid__to_permissions_table.php
+++ modules/admin/database/migrations/2017_10_23_115237_add_routename_pid__to_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddRoutenameFidToPermissionsTable extends Migration
+class AddRoutenamePidToPermissionsTable extends Migration
 {
     /**
      * Run the migrations.
