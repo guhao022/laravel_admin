c46c631199c88486349293b50a5f62ba06225523
guhao <378999587@qq.com>
2017-12-25 17:29:48 +0800

    fix

--- src/Repositories/AdminRepository.php
+++ src/Repositories/AdminRepository.php
@@ -113,6 +113,7 @@ class AdminRepository
                 }
             }
             if($needNotify){
+
                 $admin->notify(new PermissionNotification($newRoles));
             }
         }
