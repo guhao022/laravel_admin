4758716587192d5fc647ed07ea1f8743e002211c
guhao <378999587@qq.com>
2017-12-25 17:27:21 +0800

    fix

--- src/Repositories/AdminRepository.php
+++ src/Repositories/AdminRepository.php
@@ -96,6 +96,7 @@ class AdminRepository
             }
 
             $hasRoleIds = [];
+
             foreach($admin->roles as $adminRole){
                 array_push($hasRoleIds,$adminRole->id);
             }
